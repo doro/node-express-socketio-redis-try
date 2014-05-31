2cec731e2ba00e565483c601f9f7afd880858704
doron rotem <doronrotem@gmail.com>
2014-05-31 14:14:14 +0300

    refactor: encapsulate client server comm layer

--- public/index.js
+++ public/index.js
@@ -1,17 +1,51 @@
 $(document).ready(function() {
 
-  var server = io.connect(location.protocol + '//' + location.host); 
+  var cbPop = function(msg) {
+    if (msg != null) $('#messages').prepend("<p>" + msg + "</p>");
+  };
+
 
   $('#popBtn').on('click', function() {
-    server.emit('messages-pop', "get");
+    server.pop();
   });
 
   $('#pushBtn').on('click', function() {
     var msg = $('#text').val();
-    server.emit('messages-push', msg);
+    server.push(msg);
   });
 
-  server.on('messages', function (data) {
-    if (data != null) $('#messages').prepend("<p>" + data + "</p>");
-  });
+  server.init(cbPop);
+
 });
+
+
+
+// abstract the server websocket api
+// server shold be init before use
+// it has 2 method: pop, push to send and get messages from the server
+var server = {
+
+  _server: null,
+  _cbPop: null,
+
+  // cbPop should recieve one param, which is the message poped
+  init: function(cbPop) {
+    server._server = io.connect(location.protocol + '//' + location.host)
+    server._cbPop = cbPop;
+
+    server._server.on('messages', function (data) {
+      server._cbPop(data);
+    });
+  },
+
+  
+  push: function(msg) {
+    server._server.emit('messages-push', msg);
+  },
+  
+  pop: function() {
+    server._server.emit('messages-pop', "get");
+  }
+  
+
+};
